33265c72b94d16c4e08548994944d9639ae67a6d
Truong-Helen <helen.truong@nevada.unr.edu>
2023-09-24 21:35:05 -0700

    Tried reducing amount of cameras to one

--- Assets/Scenes/Helen's Scene/Scripts/CameraController.cs
+++ Assets/Scenes/Helen's Scene/Scripts/CameraController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    /*
     public GameObject transitionCamera;
     public GameObject p1Camera;
     public GameObject p2Camera;
@@ -65,5 +66,66 @@ public class CameraController : MonoBehaviour
         pathP1toP2.SetActive(false);
 
 
+    }
+    */
+
+    public Transform camera;
+    public GameObject player1Arm;
+    public GameObject player2Arm;
+
+    public GameObject pathP2ToP1;
+    public GameObject pathP1ToP2;
+
+    Vector3 p1Position;
+    Vector3 p2Position;
+
+    void Start()
+    {
+        player1Arm.SetActive(false);
+        player2Arm.SetActive(false);
+
+        pathP1ToP2.SetActive(false);
+        pathP2ToP1.SetActive(false);
+
+        p1Position = new Vector3(0f, 41.14f, -48.93f);
+        p2Position = new Vector3(-0.2f, 41.2f, 33.7f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("t"))
+        {
+            if (pathP1ToP2.activeSelf)
+            {
+                SwitchToP1();
+            }
+            else
+            {
+                SwitchToP2();
+            }
+        }
+
+        if (camera.position == p1Position)
+        {
+            player1Arm.SetActive(true);
+            player2Arm.SetActive(false);
+        }
+        else if (camera.position == p2Position)
+        {
+            player1Arm.SetActive(false);
+            player2Arm.SetActive(true);
+        }
+    }
+
+    public void SwitchToP2()
+    {
+        pathP2ToP1.SetActive(false);
+        pathP1ToP2.SetActive(true);
+    }
+
+    public void SwitchToP1()
+    {
+        pathP2ToP1.SetActive(true);
+        pathP1ToP2.SetActive(false);
     }
 }
